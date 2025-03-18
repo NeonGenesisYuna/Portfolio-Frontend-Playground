9862b7e47af795caf8053e83112db00f09139902
NeonGenesisYuna <imboden.raffael@gmail.com>
2025-03-18 15:42:17 +0100

    .

--- spotify database/index.js
+++
@@ -1,7 +0,0 @@
-fetch('https://my-flask-api-ywsd.onrender.com/top-songs')
-    .then(response => response.text())  // Parse HTML response
-    .then(data => {
-        let list = document.getElementById('album-list');
-        list.innerHTML = data;  // Insert the HTML directly into the page
-    })
-    .catch(error => console.error('Error:', error));
