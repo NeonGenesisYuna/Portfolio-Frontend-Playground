359692ad338721e045a4d54f7961a8a6bdebc529
NeonGenesisYuna <imboden.raffael@gmail.com>
2025-03-25 10:25:24 +0100

    .

---
+++ README.txt
@@ -0,0 +1,41 @@
+As mentioned in the previous exam, ignore the .idea file for the same reasons stated previously.
+
+I will continue working on this site, as I've grown quite interested in webdesign, so that's why some of the sites aren't finished yet.
+
+The folder "topsecret" currently doesn't have anything of note in it, so ignore that one.
+
+The folder "spotifydatabase" was my attempt at trying to display my own spotify data (top songs/artists from the past 4 weeks) on a website with the spotify API
+and a python library (spotipy) which I haven't gotten to work properly yet. However, I've attached a file "readme_attachment.jpg", in which I got it to work with a local flask server.
+I attached the image so you can still grade the css part of it, without having to replicate the whole thing. (Also I just meshed two screenshots together,
+it actually shows the top 10 songs/artists, not just three like in the file, just to clarify).
+
+The "layers" folder isn't completed yet and the layer files don't have anything besides a background image after layer 5.
+
+The "dreams" site is also not quite finished yet, as it is still very new.
+
+And a little funfact, all the audio files in the "wired-soundtrack" site (resp. the "soundtrack" folder) are songs that I'm currently working on for an album which
+I'm planning to release when I'm done with all thirteen songs.
+
+
+
+What I didn't code on my own (or what I still don't fully understand and needed heavy help with from external sources):
+
+Every animation that was made with the .glitch class
+
+The CSS part of "wired-soundtrack" that interacts with the JS, and some of the JS part itself (I know JS doesn't get graded)
+
+
+Other small things:
+
+The scroll bar for the playlist in wired-soundtrack looks way better in firefox than in chrome.
+
+There probably is more inconsistent/conflicting code in the earlier sites I made, as I wasn't that experienced yet.
+I haven't really been able to clean that up as I've been working more on newer files as of lately.
+
+The reactive design was tested on my laptop, my monitors at home and the monitors we have here.
+The site currently doesn't look good on phones and I can't guarantee that it looks good on anything else I haven't tested yet.
+
+Sometimes the scaling on the live site was a bit off, I'm pretty sure I was able to fix all of those inconsistencies, but if something
+looks a bit off or doesn't fit the scaling of other elements, I'd maybe recommend checking the site locally.
+
+The whole "Wired" section of the website is hugely inspired by the anime "Serial Experiments Lain" and has this eerie aesthetic on purpose.
--- wired/wired_soundtrack.js
+++ wired/wired_soundtrack.js
@@ -14,7 +14,6 @@ const songs = [
     { src: "../soundtrack/Layer 13.mp3", name: "Layer 13 (Ego)" }
 ];
 
-// Create an audio element
 const audio = new Audio();
 const songNameDisplay = document.getElementById("songName");
 const vinylGif = document.getElementById("vinylGif");
@@ -22,57 +21,47 @@ const nowPlayingContainer = document.getElementById("nowPlayingContainer");
 const progressBar = document.getElementById("progressBar");
 const volumeSlider = document.getElementById("volumeSlider");
 
-// Set initial volume
 audio.volume = volumeSlider.value;
 
-// Update volume and the slider's gradient fill when changed
 volumeSlider.addEventListener("input", function () {
     audio.volume = this.value;
-    // Calculate the percentage of the slider value
+
     const percentage = ((this.value - this.min) / (this.max - this.min)) * 100;
-    // Update the CSS variable for the background gradient
+
     this.style.setProperty('--value', percentage + '%');
 });
 
-// Global variable to track the current song index
 let currentSongIndex = 0;
 
 function playSong(index) {
     currentSongIndex = index;
-    // Set the audio source and play the song
+
     audio.src = songs[index].src;
     audio.play();
 
-    // Update the song name display with the current song's name
     songNameDisplay.textContent = songs[index].name;
 
-    // Show the "Now Playing" UI (vinyl, song info, slider)
     nowPlayingContainer.style.display = "flex";
-    vinylGif.style.display = "block"; // Ensures the vinyl gif is visible
+    vinylGif.style.display = "block";
 }
 
-// Update progress bar as the song plays
 audio.addEventListener("timeupdate", function () {
     const progress = (audio.currentTime / audio.duration) * 100;
     progressBar.style.width = progress + "%";
 });
 
-// Optionally hide vinyl when the song is paused
 audio.addEventListener("pause", function () {
     vinylGif.style.display = "none";
 });
 
-// Autoplay the next song when the current one ends
 audio.addEventListener("ended", function () {
-    // Calculate the next song index, looping back to 0 if at the end
+
     const nextSongIndex = (currentSongIndex + 1) % songs.length;
     playSong(nextSongIndex);
 });
 
-// Get the playlist container from the DOM
 const playlistContainer = document.querySelector('.playlist');
 
-// Function to create and display songs dynamically
 function createPlaylist() {
     songs.forEach((song, index) => {
         const songElement = document.createElement('div');
@@ -84,19 +73,15 @@ function createPlaylist() {
         const playButton = document.createElement('button');
         playButton.textContent = "Play";
 
-        // Set the onclick event for the play button
         playButton.onclick = function() {
-            playSong(index);  // Play the song by its index
+            playSong(index);
         };
 
-        // Append the song text and button to the songElement
         songElement.appendChild(songText);
         songElement.appendChild(playButton);
 
-        // Append the songElement to the playlist container
         playlistContainer.appendChild(songElement);
     });
 }
 
-// Call the function to create the playlist when the page loads
 createPlaylist();
