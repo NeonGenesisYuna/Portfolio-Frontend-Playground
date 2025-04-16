de116ca667466bbc8d6070a1ba8386dc6db80a8f
NeonGenesisYuna <imboden.raffael@gmail.com>
2025-04-16 13:35:15 +0200

    .

---
+++ limbus_company/Sinners.sql
@@ -0,0 +1,28 @@
+INSERT INTO ids (ID, Skill1, Skill2, Skill3, Defense)
+VALUES 
+(LCB_Sinner_YiSang, Gloom, Envy, Sloth, Gloom),
+(Dieci_Associacion_YiSang, Gluttony, Lust, Sloth, Sloth),
+(LCE_EGO_Lantern_YiSang, Sloth, Envy, Gluttony, Gluttony),
+(Molar_Office_Fixer_YiSang, Lust, Sloth, Wrath, Sloth),
+(Seven_Association_South_Section_6_YiSang)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
